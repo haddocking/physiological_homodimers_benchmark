d2105348965f8d85bfbce664644d149bb56b3b8f
Brian Jimenez Garcia <b.jimenezgarcia@uu.nl>
2021-01-12 11:32:20 +0100

    PRODIGY-CRYSTAL prediction

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# physiological_homodimers_benchmark-
+# Physiological protein homodimers Benchmark
+
+## Structure
+
+* `pdbs`: folder containing for a given version (i.e. `v2`) all the PDB structures ready.
+* `prediction`: prediction files for each of the methods benchmarked.
+
+### Files
+
+* `pdbs/v2/complex.list`: list of all complexes in the benchmark
+* `pdbs/v2/physiological.list`: all complexes classified as physiological
+* `pdbs/v2/nonphysiological.list`: all complexes classified as non-physiological
+* `pdbs/v2/error.list`: Errors found on PDB structures which have been manually curated.
+
+ 
---
+++ prediction/prodigy-crystal/success_rate.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+
+def parse_prediction(file_name):
+    with open(file_name) as input_handle:
+        for line in input_handle:
+            if line.startswith('[+] Class'):
+                return line.split()[2]
+        return None
+
+
+if __name__ == "__main__":
+    complex_list_file = sys.argv[1]
+    physiological_list_file = sys.argv[2]
+    nonphysiological_list_file = sys.argv[3]
+
+    complex_list = []
+    physiological_list = []
+    nonphysiological_list = []
+
+    with open(complex_list_file) as input_handle:
+        complex_list = [line.rstrip(os.linesep) for line in input_handle.readlines() if line]
+
+    with open(physiological_list_file) as input_handle:
+        physiological_list = [line.rstrip(os.linesep) for line in input_handle.readlines() if line]
+
+    with open(nonphysiological_list_file) as input_handle:
+        nonphysiological_list = [line.rstrip(os.linesep) for line in input_handle.readlines() if line]
+
+    print(f'Size of complex list: {len(complex_list)}')
+    print(f'Size of Physiological list: {len(physiological_list)}')
+    print(f'Size of Non Physiological list: {len(nonphysiological_list)}')
+
+    bio_predicted = []
+    crystal_predicted = []
+
+    for pdb_id in complex_list:
+        prediction_out = f'{pdb_id}.out'
+        prediction = parse_prediction(prediction_out)
+        if not prediction:
+            raise SystemExit(f'Prediction not found for {prediction_out}')
+        elif prediction == 'BIO':
+            bio_predicted.append(pdb_id)
+        else:
+            crystal_predicted.append(pdb_id)
+
+    print(f'Predicted as BIO and Physiological: {len(set(bio_predicted) & set(physiological_list))}')
+    print(f'Predicted as BIO and Non-Physiological: {len(set(bio_predicted) & set(nonphysiological_list))}')
+    print(f'Predicted as CRYSTAL and Physiological: {len(set(crystal_predicted) & set(physiological_list))}')
+    print(f'Predicted as CRYSTAL and Non-Physiological: {len(set(crystal_predicted) & set(nonphysiological_list))}')
+    print()
+    success_rate = (len(set(bio_predicted) & set(physiological_list)) + len(set(crystal_predicted) & set(nonphysiological_list))) / len(complex_list)
+    print(f'Success rate: {round(success_rate, 2)}%')
